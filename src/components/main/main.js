7cbd4141e3e584174f98e111170be6f1d3f97f7f
Marian <mariana_balsamo@hotmail.com>
2019-09-28 23:18:01 -0300

    maldito git

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Note from '../note/note';
+<<<<<<< HEAD
 
 import './Main.css';
 
@@ -56,4 +57,46 @@ class  Main extends Component {
 }
 
 
+=======
+import './Main.css';
+
+class Main extends Component {
+
+  state = {
+    note: '',
+    notesArray: [],
+		showNotes: false,      
+		}
+
+
+  agregar = () =>{
+    const nuevoArray = [...this.state.arrayDeNotas]
+    nuevoArray.push(this.state.nota);
+
+    this.setState({
+      arrayDeNotas:nuevoArray,
+      mostrarNotas:true,
+      nota:''
+    })
+  }
+
+  handleChange = e =>{
+  	this.setState({
+      nota:e.target.value
+    })
+	}
+
+  render(){
+    return (
+      <div className="background">
+        <div>To Do List</div>
+        <input value={this.state.nota} onChange={this.handleChange}></input>
+        <button onClick={this.agregar}>Agregar</button>
+        	{this.state.mostrarNotas === true ? <Note array = {this.state.arrayDeNotas} funcion={this.funcionDelPadre}/> : ''}
+    	</div>
+  	)
+  }
+}
+
+>>>>>>> f245ea969203577b936e8b3537a1dc052a3fc6e8
 export default Main;
